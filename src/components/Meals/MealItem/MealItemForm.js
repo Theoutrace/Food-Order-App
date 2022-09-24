326e26f1c2c92ab0ae9126db0b7296bae92e66fd
Theoutrace <prakashkumar.imw@gmail.com>
2022-09-24 09:37:42 +0530

    passing dynamic id to the input

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -2,16 +2,19 @@ import React from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
   return (
     <form className={classes.form}>
-      <Input label='Amount' input={{
-        id: 'amount',
-        type: 'number',
-        min: '1',
-        max: '5',
-        defaultValue: '1',
-      }} />
+      <Input
+        label="Amount"
+        input={{
+          id: "amount" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
   );
